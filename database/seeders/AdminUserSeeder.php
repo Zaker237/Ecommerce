0bca1aaab6faf40be61ffc25fa01a20e27a90ba6
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-03 16:41:03 +0100

    Admin seeder and is admin migration

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@ecommerce.app',
+            'password' => bcrypt('admin123'),
+            'email_verified_at' => now(),
+            'is_admin' => true
+        ]);
+    }
+}
