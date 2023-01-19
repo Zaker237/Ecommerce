b63839418f5879ac9a2006ec3850a16c2daae6e8
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-19 04:59:55 +0100

    update user type

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,7 +1,7 @@
 export interface IUser {
-	id: null | number;
-	name: null  | string;
-	email: null  | string;
+	id?: null | number;
+	name?: null  | string;
+	email?: null  | string;
 }
 
 export interface IUserLogin {
@@ -9,3 +9,16 @@ export interface IUserLogin {
   password: string | null;
   remember?: boolean
 }
+
+export interface ILoginResponse {
+  user: IUser,
+  token: string
+}
+
+export type RootUserState = {
+  sUser: IUser;
+  sIsAdmin?: true | null;
+  sToken: string | null;
+  loading: boolean,
+  error: null | string
+};
