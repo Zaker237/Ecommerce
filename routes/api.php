71a8771b5eb9c5db5bcf90c3c9e1192affcebd59
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-19 05:27:42 +0100

    add product route resource to api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'admin'])->group(function(){
+    // user routes
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    // product routes
+    Route::apiResource('products', ProductController::class);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
