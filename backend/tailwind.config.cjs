99e32ebef9a71d81769799cd426b72764978caca
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-01-19 05:31:48 +0100

    update tailwind configs

--- backend/tailwind.config.cjs
+++ backend/tailwind.config.cjs
@@ -21,7 +21,23 @@ module.exports = {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          "from": {
+            transform: "translateY(-0.75rem)",
+            opacity: '0'
+          },
+          "to": {
+            transform: "translateY(0rem)",
+            opacity: '1'
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': "fade-in-down 0.2s ease-in-out both",
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
