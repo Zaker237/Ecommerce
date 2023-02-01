f9678d510cb2adca52b51b6873655bc106ed814d
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-01 13:12:22 +0100

    login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,45 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+<x-app-layout>
+    <form method="POST" action="{{ route('login') }}" class="w-full sm:w-[400px] px-5 mx-auto mt-15 text-center">
+        <h2 class="text-2xl font-semibold mb-2">Login o your account</h2>
+        <p class="mb-4">
+            or
+            <a
+                href="{{ route('register') }}"
+                class="text-purple-600 hover:text-purple-500"
+            >
+                create new account
             </a>
-        </x-slot>
+        </p>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <x-auth-session-status class="mb-4" :status="session('status')"/>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        @csrf
+        <div class="mb-3">
+            <x-input type="email" name="email" placeholder="Your email address" :value="old('email')"/>
+        </div>
+        <div class="mb-3">
+            <x-input type="password" name="password" placeholder="Your password" :value="old('password')" />
+        </div>
+        <div class="flex justify-between mb-3">
+            <div class="flex items-center">
+                <input
+                    id="loginRememberMe"
+                    type="checkbox"
+                    class="text-purple-600 focus:ring-purple-600 mr-2"
+                />
+                <label for="loginRememberMe" class="text-gray-700">Remember Me</label>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}" class="text-purple-600 hover:text-purple-500">
+                    Forgot Password?
+                </a>
+            @endif
+        </div>
+        <button
+            class="flex items-center justify-center btn-emerald bg:bg-emerald-600 w-full mt-3"
+        >
+            Login
+        </button>
+    </form>
+</x-app-layout>
