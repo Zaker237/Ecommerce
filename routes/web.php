514ec7939edd1527c690133167a298892171b0a5
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-04 03:08:12 +0100

    add cart items routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'index'])->name('home');
-Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
+Route::middleware(['guestOrVerified'])->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+    Route::get('/product/{product:slug}', [ProductController::class, 'view'])->name('product.view');
+
+    Route::prefix('/cart')->name('cart.')->group(function() {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::post('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::post('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::post('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('update-quantity');
+    });
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
