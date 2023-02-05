a2da91b4744291b30e69acc48c4e90ecddbe4e49
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-05 14:15:44 +0100

    create and register Seeder for country

---
+++ database/seeders/CountrySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use PHPUnit\Framework\Constraint\Count;
+
+class CountrySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usaStates = [
+            "AL" => 'Alabama',
+            "AK" => 'Alaska',
+            "AZ" => 'Arizona',
+            "AR" => 'Arkansas',
+            "CA" => 'California',
+        ];
+        $countries = [
+            ['code' => 'geo', 'name' => 'Georgia', 'states' => null],
+            ['code' => 'ind', 'name' => 'India', 'states' => null],
+            ['code' => 'usa', 'name' => 'United States of America', 'states' => json_encode($usaStates)],
+            ['code' => 'ger', 'name' => 'Germany', 'states' => null],
+            ['code' => 'cm', 'name' => 'Cameroon', 'states' => null],
+            ['code' => 'fr', 'name' => 'France', 'states' => null],
+        ];
+        Country::insert($countries);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             ProductSeeder::class,
+            CountrySeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
     }
