ec7c5e29df2865d211756f51a41555db0331e1d2
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-06 11:55:22 +0100

    mange the relationship between User, Customer and CustomerAddress Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Enums\AddressType;
 
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['first_name', 'last_name', 'phone', 'status',];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'id');
+    }
+
+    private function _getAddresses(): HasOne
+    {
+        return $this->hasOne(CustomerAddress::class, 'customer_id', 'id');
+    }
+
+    public function shippingAddress(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Shipping);
+    }
+
+    public function billingAddress(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Billing);
+    }
 }
