aab4f4c204c6c20b5115d4ffe3f810e4e757b066
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-07 18:12:12 +0100

    Add PaymentStutus and OrderStatus as Enums

---
+++ app/Enums/OrderStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+class OrderStatus
+{
+    public const Unpaid = 'unpaid';
+    public const Paid = 'paid';
+    public const Complited = 'complete';
+};
---
+++ app/Enums/PaymentStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+class PaymentStatus
+{
+    public const Pending = 'pending';
+    public const Paid = 'paid';
+    public const Failed = 'failed';
+};
