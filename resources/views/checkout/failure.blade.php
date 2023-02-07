24ca734707db92293ffe2a7169e91f24ea7d45b8
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-07 18:38:44 +0100

    checkout success and failure view

---
+++ resources/views/checkout/failure.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="w-[400px] mx-auto bg-red-500 py-2 px-3 text-white rounded">
+        <h1>your Payment was nor successful</h1>
+        <p>{{ $message ?? '' }}</p>
+    </div>
+</x-app-layout>
