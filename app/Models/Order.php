4392e60bfdf0c511f835ad9517d5f7c2c43a9f09
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-08 17:29:31 +0100

    Implement Order and OrderItem Models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,10 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use \App\Enums\OrderStatus;
 
 class Order extends Model
 {
     use HasFactory;
 
     protected $fillable = ['status', 'total_price', 'created_by', 'updated_by'];
+
+    public function isPaid()
+    {
+        return $this->status == OrderStatus::Paid;
+    }
+
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'unit_price'
+    ];
+
+    public function oder(): BelongsTo
+    {
+        return $this->belongsTo(Oder::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
