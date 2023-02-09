2696751efa4c28f5cf838ba6c11e2f90c36e8fe2
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-09 18:25:18 +0100

    Order management on the dasboard

--- backend/src/views/orders/OrderStatus.vue
+++ backend/src/views/orders/OrderStatus.vue
@@ -1,3 +1,18 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+const {order} = defineProps({
+  order: Object
+})
+</script>
+
+<template>
+	<span class="text-white py-1 px-2 rounded " :class="{
+    'bg-emerald-500': ['paid', 'completed'].includes(order.status),
+    'bg-orange-400': order.status === 'shipped',
+    'bg-red-500': order.status === 'cancelled',
+    'bg-gray-400': order.status === 'unpaid',
+  }">
+		{{ order.status }}
+	</span>
+</template>
+
 <style scoped></style>
--- backend/src/views/orders/Orders.vue
+++ backend/src/views/orders/Orders.vue
@@ -1,3 +1,19 @@
-<script setup lang="ts"></script>
-<template>Orders</template>
+<script setup lang="ts">
+import {computed, onMounted, ref} from "vue";
+import store from "../../store";
+import OrdersTable from "./OrdersTable.vue";
+
+const orders = computed(() => store.state.orders);
+const showOrder = (p) => {
+
+}
+</script>
+
+<template>
+	<div class="flex items-center justify-between mb-3">
+    <h1 class="text-3xl font-semibold">Orders</h1>
+  </div>
+  <OrdersTable @clickView="viewOrder"/>
+</template>
+
 <style scoped></style>
