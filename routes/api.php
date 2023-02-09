2c0c07ab1d8243e24cf3a5090ab1ffccb37f6b85
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-09 18:40:01 +0100

    routes for Orders Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,12 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function(){
 
     // product routes
     Route::apiResource('products', ProductController::class);
+
+    // order routes
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::get('orders/statuses', [OrderController::class, 'getStatuses']);
+    Route::post('orders/change-status/{order}/{status}', [OrderController::class, 'changeStatus']);
+    Route::get('orders/{order}', [OrderController::class, 'view']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
