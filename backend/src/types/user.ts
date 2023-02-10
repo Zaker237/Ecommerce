dbc58004f2d6b5dd010e40fd03a223e0cbd4e028
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-10 19:46:47 +0100

    users types

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -1,7 +1,10 @@
+import { ILink, IMeta} from "./commons";
+
 export interface IUser {
-	id?: null | number;
-	name?: null  | string;
-	email?: null  | string;
+	id?: | number;
+	name?: | string;
+  email?: | string;
+  created_at?: string;
 }
 
 export interface IUserLogin {
@@ -15,10 +18,33 @@ export interface ILoginResponse {
   token: string
 }
 
+export type GetUserResponse = {
+  data: IUser;
+  links: ILink;
+  meta: IMeta;
+};
+
+export type GetUsersResponse = {
+  data: IUser[];
+  links: ILink;
+  meta: IMeta;
+};
+
+export type GetUsersParams = {
+  url?: string | null;
+  search?: string;
+  per_page?: number;
+  sort_field?: string;
+  sort_direction?: string
+};
+
 export type RootUserState = {
-  sUser: IUser;
+  users: IUser[];
+  currentUser: IUser;
   sIsAdmin?: true | null;
   sToken: string | null;
+  links: ILink;
+  meta: IMeta;
   loading: boolean,
   error: null | string
 };
