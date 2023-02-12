8e68c0daf62dbee6fd18bf3c868105315e0562d6
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-12 07:14:22 +0100

    Fix CustomInput. add support for checkbox and select

--- backend/src/components/core/CustomInput.vue
+++ backend/src/components/core/CustomInput.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import {computed} from "vue";
+import {computed, ref} from "vue";
+
 const props = defineProps({
-  modelValue: [String, Number, File],
+  modelValue: [String, Number, Boolean, File],
   label: String,
   type: {
     type: String,
@@ -16,7 +17,13 @@ const props = defineProps({
   append: {
     type: String,
     default: ''
-  }
+  },
+  selectOptions: Array
+})
+
+const id = computed(() => {
+  if (props.id) return props.id;
+  return `id-${Math.floor(1000000 + Math.random() * 1000000)}`;
 })
 const inputClasses = computed(() => {
   const cls = [
@@ -38,49 +45,58 @@ const emit = defineEmits(['update:modelValue', 'change'])
   <div>
     <label class="sr-only">{{ label }}</label>
     <div class="mt-1 flex rounded-md shadow-sm">
-      <span
-				v-if="prepend"
-        class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"
-			>
+      <span v-if="prepend"
+            class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
         {{ prepend }}
       </span>
-      <template v-if="type === 'textarea'">
-				<textarea
-					:name="name"
-					:required="required"
-					:value="props.modelValue"
-					@input="emit('update:modelValue', $event.target.value)"
-					:class="inputClasses"
-					:placeholder="label"
-				></textarea>
+      <template v-if="type === 'select'">
+        <select :name="name"
+                :required="required"
+                :value="props.modelValue"
+                :class="inputClasses"
+                @change="emit('update:modelValue', $event.target.value)">
+          <option v-for="option of selectOptions" :value="option.key">{{option.text}}</option>
+        </select>
+      </template>
+      <template v-else-if="type === 'textarea'">
+      <textarea :name="name"
+                :required="required"
+                :value="props.modelValue"
+                @input="emit('update:modelValue', $event.target.value)"
+                :class="inputClasses"
+                :placeholder="label"></textarea>
       </template>
       <template v-else-if="type === 'file'">
-        <input
-					:type="type"
-          :name="name"
-          :required="required"
-          :value="props.modelValue"
-          @input="emit('change', $event.target.files[0])"
-          :class="inputClasses"
-          :placeholder="label"
-				/>
+        <input :type="type"
+               :name="name"
+               :required="required"
+               :value="props.modelValue"
+               @input="emit('change', $event.target.files[0])"
+               :class="inputClasses"
+               :placeholder="label"/>
+      </template>
+      <template v-else-if="type === 'checkbox'">
+        <input :id="id"
+               :name="name"
+               :type="type"
+               :checked="props.modelValue"
+               :required="required"
+               @change="emit('update:modelValue', $event.target.checked)"
+               class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
+        <label :for="id" class="ml-2 block text-sm text-gray-900"> {{ label }} </label>
       </template>
       <template v-else>
-        <input
-					:type="type"
-          :name="name"
-          :required="required"
-          :value="props.modelValue"
-          @input="emit('update:modelValue', $event.target.value)"
-          :class="inputClasses"
-          :placeholder="label"
-          step="0.01"
-				/>
+        <input :type="type"
+               :name="name"
+               :required="required"
+               :value="props.modelValue"
+               @input="emit('update:modelValue', $event.target.value)"
+               :class="inputClasses"
+               :placeholder="label"
+               step="0.01"/>
       </template>
-      <span
-				v-if="append"
-        class="inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"
-			>
+      <span v-if="append"
+            class="inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
         {{ append }}
       </span>
     </div>
