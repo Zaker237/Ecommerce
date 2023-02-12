bf25c108eb05ac33fc7bd44af636caf7c2122203
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-12 08:12:42 +0100

    add CurrencyFiletr to the dashboard app

---
+++ backend/src/filters/curency.ts
@@ -0,0 +1,5 @@
+export default function currencyUSD(value: number) {
+  return new Intl.NumberFormat(
+		'en-US', {style: 'currency', currency: 'USD'}
+	).format(value);
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
+import currencyUSD from "./filters/curency";
 import App from "./App.vue";
 import router from "./router";
 
@@ -10,3 +11,7 @@ const app = createApp(App);
 app.use(store);
 app.use(router);
 app.mount("#app");
+
+app.config.globalProperties.$filters = {
+  currencyUSD
+}
