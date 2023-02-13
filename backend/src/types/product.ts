f914c849c023a6dc1d214e798e98ccadd3d47a85
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-13 19:19:48 +0100

    add published in product type

--- backend/src/types/product.ts
+++ backend/src/types/product.ts
@@ -7,6 +7,7 @@ export interface IProduct {
 	description: string | null;
   image_url?: string;
   price: number;
+  published: boolean;
   created_at: string | null;
   updated_at: string | null;
 }
