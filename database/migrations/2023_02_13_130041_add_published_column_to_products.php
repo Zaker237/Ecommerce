58a125cb391c1ce73a3503bdea53aaf9c5e17d45
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-13 14:15:57 +0100

    Migration: add published colum to product

--- database/migrations/2023_02_13_130041_add_published_column_to_products.php
+++ database/migrations/2023_02_13_130041_add_published_column_to_products.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->boolean('published')->default(false);
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('published');
         });
     }
 };
