3fba8517967264883585b5f503e1c4cf72133126
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-13 19:18:57 +0100

    Auth controller for Api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,20 +12,19 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate([
-            'email' => ['required', 'email'],
+            'email'=> ['required', 'email'],
             'password' => 'required',
             'remember' => 'boolean'
         ]);
-
         $remember = $credentials['remember'] ?? false;
         unset($credentials['remember']);
-
-        if(!Auth::attempt($credentials, $remember)){
+        if (!Auth::attempt($credentials, $remember)) {
             return response([
-                'message' => "Email or password is incorrect"
+                'message' => 'Email or password is incorrect'
             ], 422);
         }
 
+        /** @var \App\Models\User $user */
         $user = Auth::user();
         if (!$user->is_admin) {
             Auth::logout();
@@ -32,7 +32,12 @@ class AuthController extends Controller
                 'message' => 'You don\'t have permission to authenticate as admin'
             ], 403);
         }
-
+        if (!$user->email_verified_at) {
+            Auth::logout();
+            return response([
+                'message' => 'Your email address is not verified'
+            ], 403);
+        }
         $token = $user->createToken('main')->plainTextToken;
         return response([
             'user' => new UserResource($user),
