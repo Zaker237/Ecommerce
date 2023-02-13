bc08b6c70416262dda8db3dda8c4f5546405dfa1
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-13 17:16:10 +0100

    reports views

--- backend/src/views/reports/OrdersReport.vue
+++ backend/src/views/reports/OrdersReport.vue
@@ -1,3 +1,30 @@
-<script setup lang="ts"></script>
-<template></template>
+<script setup lang="ts">
+import {ref, watch, onMounted} from "vue";
+import BarChart from "../../components/core/Charts/Bar.vue";
+import {useRoute} from "vue-router";
+import { useReportStore } from "../../store/report.store";
+
+const reportStore = useReportStore();
+const route = useRoute();
+const chartData = ref([]);
+
+watch(
+	route,
+	async (rt) => {
+		await getData();
+	}
+)
+const getData = async () => {
+  chartData.value = await reportStore.getOrdersReport(route.params.date);
+}
+
+onMounted(async () => {
+	await getData();
+})
+</script>
+
+<template>
+	<BarChart :data="chartData" :height="240"/>
+</template>
+
 <style scoped></style>
--- backend/src/views/reports/Report.vue
+++ backend/src/views/reports/Report.vue
@@ -1,3 +1,48 @@
-<script setup lang="ts"></script>
-<template>Report</template>
+<script setup lang="ts">
+import {computed, ref} from "vue";
+import CustomInput from "../../components/core/CustomInput.vue";
+import {useRoute, useRouter} from "vue-router";
+import { useReportStore } from "../../store/report.store";
+
+const reportStore = useReportStore();
+const router = useRouter();
+const route = useRoute();
+
+const dateOptions = computed(() => reportStore.dateOptions);
+const chosenDate = ref('all');
+
+const onDatePickerChange = async () => {
+  await router.push({name: route.name, params: {date: chosenDate.value}})
+}
+</script>
+
+<template>
+	<div>
+    <div class="grid gap-3 grid-cols-3">
+      <div class="col-span-2 grid grid-cols-2 gap-3">
+        <router-link
+					:to="{name: 'reports.orders', params: route.params}"
+          class="bg-white py-2 px-3 text-gray-700 rounded-md text-center"
+          active-class="text-indigo-600 bg-indigo-50"
+				>
+					Orders Report
+        </router-link>
+        <router-link
+					:to="{name: 'reports.customers', params: route.params}"
+          class="bg-white py-2 px-3 text-gray-700 rounded-md text-center"
+          active-class="text-indigo-600 bg-indigo-50"
+				>
+					Customers Report
+        </router-link>
+      </div>
+      <div>
+        <CustomInput type="select" v-model="chosenDate" @change="onDatePickerChange" :select-options="dateOptions" />
+      </div>
+    </div>
+    <div class="bg-white p-3 rounded-md mt-3 shadow-md">
+      <router-view />
+    </div>
+  </div>
+</template>
+
 <style scoped></style>
