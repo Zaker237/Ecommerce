795a42952e4de67da3e9838939b7bb880ca9704b
Mboutchouang <mboutchouangalex+github@gmail.com>
2023-02-13 19:20:30 +0100

    Auth controller for Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\OrderController;
