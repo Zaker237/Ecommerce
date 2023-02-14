e2797a1a1536c0f169893998cdc59723d65fd0b9
Alex <mboutchouangalex+github@gmail.com>
2023-02-14 21:10:06 +0100

    update vite config for docker and allow javascript in tsconfig

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -7,6 +7,14 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
