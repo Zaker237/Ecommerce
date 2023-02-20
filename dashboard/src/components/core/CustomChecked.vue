10269c883eb4dbcad0feb1d29774b3b9c2cf846e
Alex <mboutchouangalex+github@gmail.com>
2023-02-20 04:29:24 +0100

    Fix: CustomCheckedInput

--- dashboard/src/components/core/CustomChecked.vue
+++ dashboard/src/components/core/CustomChecked.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed } from "vue";
 
-const emit = defineEmits(["update:modelValue", "change"]);
+const emit = defineEmits(["update"]);
 
 const props = defineProps({
-  modelValue: Boolean,
+  modelValue: [Boolean],
   label: String,
   name: String,
   required: Boolean,
-  prepend: {
-    type: String,
-    default: ''
-  },
-  append: {
-    type: String,
-    default: ''
-  },
   selectOptions: Array,
   id: {
     type: String,
@@ -30,22 +22,8 @@ const id = computed(() => {
   return `id-${Math.floor(1000000 + Math.random() * 1000000)}`;
 });
 
-const inputClasses = computed(() => {
-  const cls = [
-    `block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm`,
-  ];
-  if (props.append && !props.prepend) {
-    cls.push(`rounded-l-md`)
-  } else if (props.prepend && !props.append) {
-    cls.push(`rounded-r-md`)
-  } else if (!props.prepend && !props.append) {
-    cls.push('rounded-md')
-  }
-  return cls.join(' ')
-});
-
 const handleCheckedChange = (event: Event) => {
-  return (event.target as HTMLInputElement).checked
+  return (event.target as HTMLInputElement).checked;
 }
 </script>
 
@@ -53,28 +31,15 @@ const handleCheckedChange = (event: Event) => {
   <div>
     <label class="sr-only">{{ label }}</label>
     <div class="mt-1 flex rounded-md shadow-sm">
-      <span v-if="prepend"
-        class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"
-      >
-        {{ prepend }}
-      </span>
-      <template>
-        <input
-          :id="id"
-          :name="name"
-          type="checkbox"
-          :checked="props.modelValue"
-          :required="required"
-          @change="emit('update:modelValue', handleCheckedChange($event))"
-          class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-        />
-        <label :for="id" class="ml-2 block text-sm text-gray-900"> {{ label }} </label>
-      </template>
-      <span v-if="append"
-        class="inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 text-sm"
-      >
-        {{ append }}
-      </span>
+      <input
+        :id="id"
+        :name="name"
+        type="checkbox"
+        :checked="props.modelValue"
+        class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+        @change="emit('update', handleCheckedChange($event))"
+      />
+      <label :for="name" class="ml-2 block text-sm text-gray-900"> {{ label }} </label>
     </div>
   </div>
 </template>
